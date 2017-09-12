ada09518cf5109fd17775e2d85dbabd7bf6696bc
Klayton <kliekkio@gmail.com>
2017-09-12 16:51:41 -0700

    working omdb, need to work on twitter and spotify

--- liri.js
+++ liri.js
@@ -1,18 +1,18 @@
 var twitter = require('twitter');
-// var Spotify = require('node-spotify-api');
-// var request = require('request');
+var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+var request = require('request');
 
-// var movieName = process.argv[2];
+var movieName = process.argv[2];
 var tweets = process.argv[2];
-
-// var spotifySong = process.argv[2];
+var spotifySong = process.argv[2];
 
 // twitter client getting keys from keys.js
 var twitterClient = new Twitter({
-    consumer_key: 'UVEbCrCnCWHox4FImdq2Kd64u',
-    consumer_secret: '5EKpmaKIRcqcja6TY9dcKydDxOxNaTn9TXI6zhuJXdGY889NtS',
-    access_token_key: '907287473306296326-JBjnL7rgIEK3gysirGrgVBGVGonMTJ9',
-    access_token_secret: 'Zwof6GwV0YKHYn5uQnsBgSpLqsW1F7pl6ZtLndTLyT2tx'
+    consumer_key: keys.consumer_key,
+    consumer_secret: keys.consumer_secret,
+    access_token_key: keys.access_token_key,
+    access_token_secret: keys.acces_token_secret
   });
 
   var params = {screen_name: 'nodejs'};
@@ -22,26 +22,26 @@ var twitterClient = new Twitter({
     }
   });
 
-// // spotify keys from keys.js
-//   var spotify = new Spotify({
-//     id: keys.id,
-//     secret: keys.secret
-//   });
+// spotify keys from keys.js
+  var spotify = new Spotify({
+    id: keys.id,
+    secret: keys.secret
+  });
 
   // ombd request with key and if statement for error handling. 
-//   request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+  request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
     
       // If the request is successful (i.e. if the response status code is 200)
-    //   if (!error && response.statusCode === 200) {
+      if (!error && response.statusCode === 200) {
     
       // console logging all of the information we want from the body
-    //     console.log("The title of the film is " + JSON.parse(body).Title);
-    //     console.log("The film was released in " + JSON.parse(body).Year);
-    //     console.log("The IMBD Rating for the film is " + JSON.parse(body).imdbRating);
-    //     console.log("The Rotten Tomatoes rating is " + JSON.parse(body).Ratings[1].Value);
-    //     console.log("This film was made in " + JSON.parse(body).Country);
-    //     console.log("This film language is " + JSON.parse(body).Language);
-    //     console.log("The plot of this film is " + JSON.parse(body).Plot);
-    //     console.log("The actors in this movie are " + JSON.parse(body).Actors);
-    //   }
-    // });
+        console.log("The title of the film is " + JSON.parse(body).Title);
+        console.log("The film was released in " + JSON.parse(body).Year);
+        console.log("The IMBD Rating for the film is " + JSON.parse(body).imdbRating);
+        console.log("The Rotten Tomatoes rating is " + JSON.parse(body).Ratings[1].Value);
+        console.log("This film was made in " + JSON.parse(body).Country);
+        console.log("This film language is " + JSON.parse(body).Language);
+        console.log("The plot of this film is " + JSON.parse(body).Plot);
+        console.log("The actors in this movie are " + JSON.parse(body).Actors);
+      }
+    });
