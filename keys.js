3447069dae53bfabf5fb22254577bc4178026df2
Klayton <kliekkio@gmail.com>
2017-09-12 16:41:36 -0700

    working omdb

---
+++ keys.js
@@ -0,0 +1,16 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'UVEbCrCnCWHox4FImdq2Kd64u',
+  consumer_secret: '5EKpmaKIRcqcja6TY9dcKydDxOxNaTn9TXI6zhuJXdGY889NtS',
+  access_token_key: '907287473306296326-JBjnL7rgIEK3gysirGrgVBGVGonMTJ9',
+  access_token_secret: 'Zwof6GwV0YKHYn5uQnsBgSpLqsW1F7pl6ZtLndTLyT2tx',
+}
+
+module.exports = twitterKeys;
+
+
+var spotify = new Spotify({
+    id: b8619d1a285f4145af1f777d33cfbbf9,
+    secret: e8dc74b272ad4bc6a8dafe39866c5524
+  });
