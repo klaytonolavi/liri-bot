027cc76940dff3853d38af08e7619dd8f8a7f7c6
Klayton <kliekkio@gmail.com>
2017-09-12 17:11:51 -0700

    working on spotify

--- liri.js
+++ liri.js
@@ -1,34 +1,45 @@
-var twitter = require('twitter');
+// var twitter = require('twitter');
 var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 
-var movieName = process.argv[2];
-var tweets = process.argv[2];
-var spotifySong = process.argv[2];
+var movieName = process.argv[3];
+// var tweets = process.argv[3];
+var spotifySong = process.argv[3];
 
-// twitter client getting keys from keys.js
-var twitterClient = new Twitter({
-    consumer_key: keys.consumer_key,
-    consumer_secret: keys.consumer_secret,
-    access_token_key: keys.access_token_key,
-    access_token_secret: keys.acces_token_secret
+// var twitterClient = new Twitter({
+//     consumer_key: keys.consumer_key,
+//     consumer_secret: keys.consumer_secret,
+//     access_token_key: keys.access_token_key,
+//     access_token_secret: keys.acces_token_secret
+//   });
+ 
+  // spotify keys from keys.js
+var spotify = new Spotify({
+      id: keys.id,
+      secret: keys.secret
   });
 
-  var params = {screen_name: 'nodejs'};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      console.log(tweets);
+  // var params = {screen_name: 'nodejs'};
+  // client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  //   if (!error) {
+  //     console.log(tweets);
+  //   }
+  // });
+
+
+if (process.argv[2] === "my-tweets") {
+  // use twitter API to post last 20 tweets
+} if (process.argv[2] === "spotify-this-song") {
+  // use spotify API to post song information
+  spotify.search({spotifySong}, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
     }
+   
+  console.log(data); 
   });
-
-// spotify keys from keys.js
-  var spotify = new Spotify({
-    id: keys.id,
-    secret: keys.secret
-  });
-
-  // ombd request with key and if statement for error handling. 
+} if (process.argv[2] === "movie-this") {
   request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
     
       // If the request is successful (i.e. if the response status code is 200)
@@ -45,3 +56,8 @@ var twitterClient = new Twitter({
         console.log("The actors in this movie are " + JSON.parse(body).Actors);
       }
     });
+} if (process.argv[2] === "do-what-it-says") {
+  // taking text inside random.txt and calling command to run spotify-this-song for the song in random.txt
+}
+
+
